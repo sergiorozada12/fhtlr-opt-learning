af74940631255bf598c471b6231f524769d94326
jlorejuela <joseluis1o2c@gmail.com>
2024-12-30 17:53:30 +0100

    Battery experiments completed

--- src/experiments/battery.py
+++ src/experiments/battery.py
@@ -31,21 +31,21 @@ DISCRETIZER = Discretizer(
 )
 
 #Experiments
-EPISODES = 30_000
+EPISODES = 20_000
 BUFFER_SIZE = 1_000
 ALPHA_DQN = 0.1
 ALPHA_FHTLR_max = 0.1
-ALPHA_FHTLR_true = 0.1
-ALPHA_QL = 1.0
+ALPHA_FHTLR_true = 0.05
+ALPHA_QL = 10
 ALPHA_FHRBF = 0.1
 ALPHA_LINEAR = 0.1
 K = 50
 SCALE_max = 0.5
 SCALE_true = 0.1
-SCALE_QL = 0.5
+SCALE_QL = 0.01
 W_DECAY = 0.0
-EPS_DECAY = 0.9999
-N_EXPS = 20
+EPS_DECAY = (0.9999)**(30_000/EPISODES)
+#N_EXPS = 100
 
 def generate_env():
     env = BatteryChargingEnv(H=H)
@@ -61,58 +61,62 @@ def run_experiment_with_agent(agent_name, n_exp):
     if agent_name == "fhtlr_true":
         agent = FHTlr(DISCRETIZER, ALPHA_FHTLR_true, H, K, SCALE_true, w_decay=W_DECAY)
     if agent_name == "fhql":
-        agent = FHQLearning(DISCRETIZER, ALPHA_QL, H, SCALE_QL, 1)
+        agent = FHQLearning(DISCRETIZER, ALPHA_QL, H, SCALE_QL, 1000)
     if agent_name == "fhbf":
         agent = FHLinear(DISCRETIZER, ALPHA_LINEAR, H, BUFFER_SIZE)
     if agent_name == "fhrbf":
         agent = FHRBF(DISCRETIZER, ALPHA_FHRBF, H, BUFFER_SIZE)
+    if agent_name == "ql":
+        agent = QLearning(DISCRETIZER, ALPHA_LINEAR,0.99)
     
     Gs  = run_experiment( n=n_exp,E=EPISODES, H=H, eps=1.0, eps_decay=EPS_DECAY, env=generate_env(), agent=agent)
-    agent_file_path = f'results/agents/battery/{agent_name}/battery_{agent_name}_{n_exp}.pkl'
+    """agent_file_path = f'results/agents/battery/{agent_name}/battery_{agent_name}_{n_exp}.pkl'
     with open(agent_file_path, 'wb') as f:
-        pickle.dump(agent, f)                                
+        pickle.dump(agent, f)"""                                
     return Gs
 
-def run_paralell(names, agents,delta_exp=0):
+def run_paralell(names, agents,n_exps,delta_exp=0):
     with Pool() as pool:
-        results = pool.starmap(run_experiment_with_agent, [(agents[i], n_exp+delta_exp) for i in range(len(agents)) for n_exp in range(N_EXPS)])
+        results = pool.starmap(run_experiment_with_agent, [(agents[i], n_exp+delta_exp) for i in range(len(agents)) for n_exp in range(n_exps)])
 
     idx = 0
     for i, name in enumerate(names):
         file_path = f"results/battery_{name}.npy"
         if os.path.exists(file_path):
             existing_data = np.load(file_path)
-            combined_data = np.concatenate((existing_data, results[idx:idx + N_EXPS]), axis=0)
+            combined_data = np.concatenate((existing_data, results[idx:idx + n_exps]), axis=0)
         else:
-            combined_data = results[idx:idx + N_EXPS]
+            combined_data = results[idx:idx + n_exps]
         
         np.save(file_path, combined_data)
-        idx += N_EXPS
+        idx += n_exps
 
 def run_battery_simulations():
 
     #agents = [dqn_learner,dfhqn_learner,fhtlr_max_learner,fhtlr_true_learner,fhql,bf]
     #run_paralell(['dqn2','dfhqn2',"fhtlr2_max","fhtlr2_true"], agents)
+
+    N_EXPS = 100
+    run_paralell(['dqn-100'], ['dqn'],N_EXPS)
+    run_paralell(['dfhqn-100'], ['dfhqn'],N_EXPS)
+    run_paralell(['fhtlr_max-explore'], ['fhtlr_max'],N_EXPS)
+    run_paralell(['fhtlr_true-explore'], ['fhtlr_true'],N_EXPS)
+    run_paralell(['fhbf-explore'], ['fhbf'],N_EXPS)
+    run_paralell(['fhrbf-explore'], ['fhrbf'],N_EXPS)
+    run_paralell(['fhql-100'], ['fhql'],N_EXPS)
     
-    run_paralell(['dqn-100'], ['dqn'], N_EXPS)
-    run_paralell(['dfhqn-100'], ['dfhqn'], N_EXPS)
-    run_paralell(['fhtlr_max-100'], ['fhtlr_max'], N_EXPS)
-    """run_paralell(['fhtlr_true-100'], ['fhtlr_true'], N_EXPS)
-    run_paralell(['fhbf-100'], ['fhbf'], N_EXPS)
-    run_paralell(['fhrbf-100'], ['fhrbf'], N_EXPS)
-    
-    agents = ['fhql']
+    """agents = ['fhql']
     names = ['fhql-100']
     total_episodes = N_EXPS
-    episodes_per_run = 3 
+    episodes_per_run = 50 
 
     full_runs = total_episodes // episodes_per_run
     remaining_episodes = total_episodes % episodes_per_run
 
     for i in range(full_runs):
         print("Lote ", i)
-        run_paralell(names, agents, i * episodes_per_run)
+        run_paralell(names, agents,episodes_per_run, i * episodes_per_run)
     
     if remaining_episodes > 0:
-        run_paralell(names, agents, full_runs * episodes_per_run)"""
+        run_paralell(names, agents,remaining_episodes, full_runs * episodes_per_run)"""
 
--- src/experiments/wireless.py
+++ src/experiments/wireless.py
@@ -52,7 +52,7 @@ DISCRETIZER = Discretizer(
 )
 
 #Experiments
-EPISODES = 40_000
+EPISODES = 100_000
 BUFFER_SIZE = 1_000
 ALPHA_DQN = 0.01
 ALPHA_FHRBF = 0.1
@@ -62,7 +62,7 @@ ALPHA_QL = 10
 K = 20
 SCALE = 0.5
 W_DECAY = 0.0
-EPS_DECAY = 0.9999
+EPS_DECAY = (0.9999)**(20_000/EPISODES)
 N_EXPS = 100
 
 def generate_env():
@@ -91,61 +91,71 @@ def generate_env():
     return env
 
 def run_experiment_with_agent(agent_name, n_exp):
-    if agent_name == 'dqn2':
+    if agent_name == 'dqn':
         agent = Dqn(DISCRETIZER, ALPHA_DQN, GAMMA, BUFFER_SIZE)
-    if agent_name == 'dfhqn2':
+    if agent_name == 'dfhqn':
         agent = DFHqn(DISCRETIZER, ALPHA_DQN, H, BUFFER_SIZE)
-    if agent_name == "fhtlr2_max":
+    if agent_name == "fhtlr_max":
         agent = FHMaxTlr(DISCRETIZER, ALPHA_FHTLR, H, K, SCALE, w_decay=W_DECAY)
-    if agent_name == "fhtlr2_true":
+    if agent_name == "fhtlr_true":
         agent = FHTlr(DISCRETIZER, ALPHA_FHTLR, H, K, SCALE, w_decay=W_DECAY)
     if agent_name == "fhql":
-        agent = FHQLearning(DISCRETIZER, ALPHA_QL, H, SCALE, 1)
+        agent = FHQLearning(DISCRETIZER, ALPHA_QL, H, 0.1, 1)
     if agent_name == "fhbf":
         agent = FHLinear(DISCRETIZER, ALPHA_LINEAR, H, BUFFER_SIZE)
     if agent_name == "fhrbf":
         agent = FHRBF(DISCRETIZER, ALPHA_FHRBF, H, BUFFER_SIZE)
+    if agent_name == "ql":
+        agent = QLearning(DISCRETIZER, ALPHA_LINEAR,0.99)
     
     Gs  = run_experiment( n=n_exp,E=EPISODES, H=H, eps=1.0, eps_decay=EPS_DECAY, env=generate_env(), agent=agent)
-    agent_file_path = f'results/agents/wireles_{agent_name}_{n_exp}.pkl'
+    """agent_file_path = f'results/agents/wireless/{agent_name}/battery_{agent_name}_{n_exp}.pkl'
     with open(agent_file_path, 'wb') as f:
-        pickle.dump(agent, f)                                
+        pickle.dump(agent, f)"""                                
     return Gs
 
-def run_paralell(names, agents,delta_exp=0):
+def run_paralell(names, agents,n_exps,delta_exp=0):
     with Pool() as pool:
-        results = pool.starmap(run_experiment_with_agent, [(agents[i], n_exp+delta_exp) for i in range(len(agents)) for n_exp in range(N_EXPS)])
+        results = pool.starmap(run_experiment_with_agent, [(agents[i], n_exp+delta_exp) for i in range(len(agents)) for n_exp in range(n_exps)])
 
     idx = 0
     for i, name in enumerate(names):
-        file_path = f"results/{name}.npy"
+        file_path = f"results/battery_{name}.npy"
         if os.path.exists(file_path):
             existing_data = np.load(file_path)
-            combined_data = np.concatenate((existing_data, results[idx:idx + N_EXPS]), axis=0)
+            combined_data = np.concatenate((existing_data, results[idx:idx + n_exps]), axis=0)
         else:
-            combined_data = results[idx:idx + N_EXPS]
+            combined_data = results[idx:idx + n_exps]
         
         np.save(file_path, combined_data)
-        idx += N_EXPS
+        idx += n_exps
 
 def run_wireless_simulations():
 
     #agents = [dqn_learner,dfhqn_learner,fhtlr_max_learner,fhtlr_true_learner,fhql,bf]
     #run_paralell(['dqn2','dfhqn2',"fhtlr2_max","fhtlr2_true"], agents)
+
+    N_EXPS = 100
+    """run_paralell(['dqn-100'], ['dqn'],N_EXPS)
+    run_paralell(['dfhqn-100'], ['dfhqn'],N_EXPS)"""
+    run_paralell(['fhtlr_max-explore'], ['fhtlr_max'],N_EXPS)
+    run_paralell(['fhtlr_true-explore'], ['fhtlr_true'],N_EXPS)
+    """run_paralell(['fhbf'], ['fhbf'],N_EXPS)
+    run_paralell(['fhrbf'], ['fhrbf'],N_EXPS)
+    run_paralell(['fhql-100'], ['fhql'],N_EXPS)
     
-    agents = ['fhrbf']
-    names = ['fhrbf-100']
-    total_episodes = 100
-    episodes_per_run = N_EXPS 
+    agents = ['fhql']
+    names = ['fhql-100']
+    total_episodes = N_EXPS
+    episodes_per_run = 50 
 
     full_runs = total_episodes // episodes_per_run
     remaining_episodes = total_episodes % episodes_per_run
 
     for i in range(full_runs):
         print("Lote ", i)
-        run_paralell(names, agents, i * episodes_per_run)
+        run_paralell(names, agents,episodes_per_run, i * episodes_per_run)
     
     if remaining_episodes > 0:
-        run_paralell(names, agents, full_runs * episodes_per_run)
-    
-    plot_wireless()
+        run_paralell(names, agents,remaining_episodes, full_runs * episodes_per_run)"""
+
